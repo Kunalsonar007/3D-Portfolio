2efaff780d28930d19591a92657b4ab9d1c1d997
Kunalsonar007 <kunalwankhede958@gmail.com>
2024-11-24 13:01:24 +0530

    New Commit

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -172,7 +172,7 @@ const HeroBanner = () => {
           >
             <img src={externalLinkIcon} alt="" className="w-[15px]" />
             <div className="text-black">
-              <a href="https://drive.google.com/file/d/1CrLi5woPHd4H7B8mJac5nFv3oFnnkQuR/view" target="_blank" download="resume.pdf">
+              <a href="https://drive.google.com/file/d/1C1SLkJrSgBVxYxjJWl6DaaC4lYL6bmA2/view?usp=sharing" target="_blank" download="resume.pdf">
                 Resume
               </a>
             </div>
